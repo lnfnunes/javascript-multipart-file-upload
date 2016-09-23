942bc7021413966910a9e50850102edf15ee0657
Marcos Vallim <mvallim@cit014432>
2016-09-22 22:46:28 -0300

    Creating example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 'use strict';
 
-var CONFIG = require('./config.json');
-
-var options = require('./values/options.js');
-
 angular
     .module(CONFIG.name, ['s3uploadmultipart'])
     .value('s3FileUploadMultipartOptions', options)
+    .directive('s3uploadmultipart', function() {
+        return { 
+            restrict: 'E',
+            templateUrl: 'views/angular-s3-fileupload-multipart.html',
+            controller: 's3uploadmultipartcontroller',
+            scope: {
+                fileupload: '='
+            }
+        };
+    })
+    .controller('s3uploadmultipartcontroller', ['$scope', function($scope) {
+        $scope.$watch('fileupload', function(value) {
+            console.log(value);
+        })
+    }])
     .run([
         function() {
         }
